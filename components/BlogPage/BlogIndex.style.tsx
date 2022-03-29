85873faf8bde6a71707dd23ec59a71b8585208b7
Yohanes Setiawan <test27c@gmail.com>
2022-03-28 18:21:14 -0700

    add filter on blog page

--- components/BlogPage/BlogIndex.style.tsx
+++ components/BlogPage/BlogIndex.style.tsx
@@ -14,3 +14,23 @@ export const BlogIntroLink = styled.a`
   border-bottom: ${theme.variants.link.borderBottom};
   font-family: ${theme.fonts.link};
 `;
+
+export const BlogIntroductionContainer = styled.div`
+  a {
+    margin-left: 5px;
+  }
+`;
+
+export const BlogLayoutSearchBar = styled.input`
+  margin-bottom: 20px;
+  padding: 10px;
+  font-size: 16px;
+  border: none;
+  box-shadow: rgb(222, 222, 222) 0px 0px 10px 1px;
+  border-radius: 5px;
+  min-width: 300px;
+
+  &:focus {
+    outline: none;
+  }
+`;
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { getSortedPostsData } from "../../lib/posts";
 import { Heading } from "../../components/Util.style";
 import BlogCard from "../../components/BlogCard/BlogCard";
-import { BlogIndexContainer } from "../../components/BlogPage/BlogIndex.style";
+import {
+  BlogIndexContainer,
+  BlogIntroductionContainer,
+  BlogLayoutSearchBar,
+} from "../../components/BlogPage/BlogIndex.style";
 import { IBlogPost } from "../../interfaces/Blog";
 import { GetServerSideProps } from "next";
 import BlogTags from "../../components/BlogPage/BlogTags";
@@ -14,7 +18,12 @@ export default function Blog({
   posts: IBlogPost[];
   qs: { tags: string | null; searchQuery: string | null };
 }) {
-  const renderPosts = posts.map((post) => (
+  const [searchKeyword, setSearchKeyword] = useState("");
+  const filteredPosts: IBlogPost[] = posts.filter((post) =>
+    post.title.toLowerCase().trim().includes(searchKeyword.toLowerCase().trim())
+  );
+
+  const renderPosts = filteredPosts.map((post) => (
     <div key={post.id}>
       <BlogCard key={post.id} {...post} />
     </div>
@@ -22,40 +31,51 @@ export default function Blog({
 
   return (
     <BlogIndexContainer>
-      <Heading>Blog posts on my experience as a software engineer ✍🏼</Heading>
-      <p>
-        I&apos;m trying to solidify my learnings and help others at the same
-        time by writing these short blog posts. I generally write about problems
-        I come across and how I solved them. I&apos;ll occassionally also write
-        about my personal experiences of navigating my career as a software
-        engineer. If you spot an error, or have any comments, suggestions or
-        questions about what I&apos;ve written, contact me on Twitter
-        <a
-          href="https://twitter.com/yohanes_dev"
-          target="_blank"
-          rel="noreferrer"
-        >
-          @yohanes_dev
-        </a>
-        ,
-        <a
-          href="https://www.instagram.com/yohaness.setiawann/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Instagram
-        </a>
-        , or email
-        <a
-          href="mailto:yohanessetiawan.us@gmail.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-          hello at yohanessetiawan.us@gmail.com
-        </a>
-        . I&apos;d love to hear from you. 🤓
-      </p>
-      <BlogTags {...qs} />
+      <BlogIntroductionContainer>
+        <Heading>Blog posts on my experience as a software engineer ✍🏼</Heading>
+        <p>
+          I&apos;m trying to solidify my learnings and help others at the same
+          time by writing these short blog posts. I generally write about
+          problems I come across and how I solved them. I&apos;ll occassionally
+          also write about my personal experiences of navigating my career as a
+          software engineer. If you spot an error, or have any comments,
+          suggestions or questions about what I&apos;ve written, contact me on
+          Twitter
+          <a
+            href="https://twitter.com/yohanes_dev"
+            target="_blank"
+            rel="noreferrer"
+          >
+            @yohanes_dev
+          </a>
+          ,
+          <a
+            href="https://www.instagram.com/yohaness.setiawann/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Instagram
+          </a>
+          , or email
+          <a
+            href="mailto:yohanessetiawan.us@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            yohanessetiawan.us@gmail.com
+          </a>
+          . I&apos;d love to hear from you. 🤓
+        </p>
+      </BlogIntroductionContainer>
+      {qs.tags ? (
+        <BlogTags {...qs} />
+      ) : (
+        <BlogLayoutSearchBar
+          value={searchKeyword}
+          onChange={(input) => setSearchKeyword(input.target.value)}
+          placeholder="Search Blog Posts"
+        />
+      )}
       {renderPosts}
     </BlogIndexContainer>
   );
