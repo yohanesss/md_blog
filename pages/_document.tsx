abea5ed195c1c8e3573adfcedb4c4fbfa9e3b434
Yohanes Setiawan <test27c@gmail.com>
2022-03-29 10:26:55 -0700

    update context _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,19 +3,20 @@ import Document, {
   Head,
   Main,
   NextScript,
+  DocumentContext,
   // DocumentContext,
 } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: any) {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App: any) => (props: any) =>
+          enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />),
         });
 
