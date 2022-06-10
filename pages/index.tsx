a78b94c6f29966afa8298780bf8db3454b3faaee
Yohanes Setiawan <test27c@gmail.com>
2022-06-10 10:54:44 -0700

    update styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -50,6 +50,7 @@ const Home: NextPage<HomeProps> = ({ latestPosts }) => {
               margin: "auto",
               width: "fit-content",
               textDecoration: "none",
+              color: "inherit",
             }}
           >
             View More
